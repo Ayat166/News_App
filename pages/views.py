2c153f0ce2eaf4e89c4ce3ca3de56f114dd7c4cb
Ayat Ali <ayatali1661@gmail.com>
2022-11-03 19:19:06 +0200

    initial commit

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,35 @@ import requests
 # Create your views here.
 
 def index(request):   
+       r = requests.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=bbb83246743a4927bee52dcc7e7fb48e')
+       res = r.json()
+       data = res['articles']
+       title = []
+       description = []
+       image = []
+       url = []
+       newid=[]
+       content=[]
+       c=0
+       for i in data:
+           title.append(i['title'])
+           description.append(i['description'])
+           image.append(i['urlToImage'])
+           url.append(i['url'])
+           content.append(i['content'])
+           newid.append(c)
+           c+=1
+       news = zip(title,description,image,url,newid,content)
+       #request.session['news'] = news
+
+       tobic="Headlines"
+       context={
+            'news':news,
+            'tobic':tobic
+        }
+       return render(request,'pages/index.html',context)
+    
+def search(request):
     if 'search' in request.GET and 'btnsearch' in request.GET:
         search=request.GET['search']
         SEARCH_NEWS = "https://newsapi.org/v2/everything?apiKey=bbb83246743a4927bee52dcc7e7fb48e&q="
@@ -14,29 +43,26 @@ def index(request):
         description = []
         image = []
         url = []
+        content=[]
+        tobic="Search"
+        name = search
+        newid=[] 
+        c=0
         for i in data:
            title.append(i['title'])
            description.append(i['description'])
            image.append(i['urlToImage'])
            url.append(i['url'])
-        news = zip(title,description,image,url)
-        return render(request,'pages/index.html',{'news':news})
-    else:
-       r = requests.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=bbb83246743a4927bee52dcc7e7fb48e')
-       res = r.json()
-       data = res['articles']
-       title = []
-       description = []
-       image = []
-       url = []
-       for i in data:
-           title.append(i['title'])
-           description.append(i['description'])
-           image.append(i['urlToImage'])
-           url.append(i['url'])
-       news = zip(title,description,image,url)
-       return render(request,'pages/index.html',{'news':news})
-    
+           content.append(i['content'])
+           newid.append(c)
+           c+=1
+        news = zip(title,description,image,url,newid,content)
+        context={
+            'news':news,
+            'tobic':tobic,
+            'name':name
+        }
+        return render(request,'pages/index.html',context)
     
 def genral(request):
        r = requests.get('https://newsapi.org/v2/top-headlines?category=general&country=in&apiKey=bbb83246743a4927bee52dcc7e7fb48e')
@@ -46,13 +72,24 @@ def genral(request):
        description = []
        image = []
        url = []
+       newid=[]
+       content=[]
+       c=0
        for i in data:
            title.append(i['title'])
            description.append(i['description'])
            image.append(i['urlToImage'])
            url.append(i['url'])
-       news = zip(title,description,image,url)
-       return render(request,'pages/index.html',{'news':news})
+           newid.append(c)
+           content.append(i['content'])
+           c+=1
+       news = zip(title,description,image,url,newid,content)
+       tobic="General"
+       context={
+            'news':news,
+            'tobic':tobic
+        }
+       return render(request,'pages/index.html',context)
    
 def business(request):
        r = requests.get('https://newsapi.org/v2/top-headlines?category=business&country=in&apiKey=bbb83246743a4927bee52dcc7e7fb48e')
@@ -62,13 +99,24 @@ def business(request):
        description = []
        image = []
        url = []
+       newid=[]
+       content=[]
+       c=0
        for i in data:
            title.append(i['title'])
            description.append(i['description'])
            image.append(i['urlToImage'])
            url.append(i['url'])
-       news = zip(title,description,image,url)
-       return render(request,'pages/index.html',{'news':news})
+           newid.append(c)
+           content.append(i['content'])
+           c+=1
+       news = zip(title,description,image,url,newid,content)
+       tobic="Business"
+       context={
+            'news':news,
+            'tobic':tobic
+        }
+       return render(request,'pages/index.html',context)
    
 def sport(request):
        r = requests.get('https://newsapi.org/v2/top-headlines?category=sport&country=in&apiKey=bbb83246743a4927bee52dcc7e7fb48e')
@@ -78,13 +126,24 @@ def sport(request):
        description = []
        image = []
        url = []
+       newid=[]
+       content=[]
+       c=0
        for i in data:
            title.append(i['title'])
            description.append(i['description'])
            image.append(i['urlToImage'])
            url.append(i['url'])
-       news = zip(title,description,image,url)
-       return render(request,'pages/index.html',{'news':news})
+           newid.append(c)
+           content.append(i['content'])
+           c+=1
+       news = zip(title,description,image,url,newid,content)
+       tobic="Sport"
+       context={
+            'news':news,
+            'tobic':tobic
+        }
+       return render(request,'pages/index.html',context)
    
 def technology(request):
        r = requests.get('https://newsapi.org/v2/top-headlines?category=technology&country=in&apiKey=bbb83246743a4927bee52dcc7e7fb48e')
@@ -94,13 +153,24 @@ def technology(request):
        description = []
        image = []
        url = []
+       newid=[]
+       content=[]
+       c=0
        for i in data:
            title.append(i['title'])
            description.append(i['description'])
            image.append(i['urlToImage'])
            url.append(i['url'])
-       news = zip(title,description,image,url)
-       return render(request,'pages/index.html',{'news':news})
+           newid.append(c)
+           content.append(i['content'])
+           c+=1
+       news = zip(title,description,image,url,newid,content)
+       tobic="Technology"
+       context={
+            'news':news,
+            'tobic':tobic
+        }
+       return render(request,'pages/index.html',context)
 
 def entertainment(request):
        r = requests.get('https://newsapi.org/v2/top-headlines?category=entertainment&country=in&apiKey=bbb83246743a4927bee52dcc7e7fb48e')
@@ -110,13 +180,192 @@ def entertainment(request):
        description = []
        image = []
        url = []
+       newid=[]
+       content=[]
+       c=0
        for i in data:
            title.append(i['title'])
            description.append(i['description'])
            image.append(i['urlToImage'])
            url.append(i['url'])
-       news = zip(title,description,image,url)
-       return render(request,'pages/index.html',{'news':news})
+           newid.append(c)
+           content.append(i['content'])
+           c+=1
+       news = zip(title,description,image,url,newid,content)
+       tobic="Entertainment"
+       context={
+            'news':news,
+            'tobic':tobic
+        }
+       return render(request,'pages/index.html',context)
    
          
-        
+def new(request,new_id):
+       r = requests.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=bbb83246743a4927bee52dcc7e7fb48e')
+       res = r.json()
+       data = res['articles']
+       title = []
+       description = []
+       image = []
+       url = []
+       newid=[]
+       content=[]
+       c=0
+       for i in data:
+         if(int(new_id)==c):
+           title.append(i['title'])
+           description.append(i['description'])
+           image.append(i['urlToImage'])
+           url.append(i['url'])
+           content.append(i['content'])
+           newid.append(c)
+           break
+         c+=1
+       news = zip(title,description,image,url,newid,content)
+
+       context={
+            'news':news,
+       }
+       return render(request,'pages/new.html',context)
+   
+def newBusiness (request,new_id):
+       r = requests.get('https://newsapi.org/v2/top-headlines?category=business&country=in&apiKey=bbb83246743a4927bee52dcc7e7fb48e')
+       res = r.json()
+       data = res['articles']
+       title = []
+       description = []
+       image = []
+       url = []
+       newid=[]
+       content=[]
+       c=0
+       for i in data:
+         if(int(new_id)==c):
+           title.append(i['title'])
+           description.append(i['description'])
+           image.append(i['urlToImage'])
+           url.append(i['url'])
+           newid.append(c)
+           content.append(i['content'])
+           break
+         c+=1
+       news = zip(title,description,image,url,newid,content)
+
+       context={
+            'news':news,
+       }
+       return render(request,'pages/new.html',context)
+   
+def newSport (request,new_id):
+       r = requests.get('https://newsapi.org/v2/top-headlines?category=sport&country=in&apiKey=bbb83246743a4927bee52dcc7e7fb48e')
+       res = r.json()
+       data = res['articles']
+       title = []
+       description = []
+       image = []
+       url = []
+       newid=[]
+       content=[]
+       c=0
+       for i in data:
+         if(int(new_id)==c):
+           title.append(i['title'])
+           description.append(i['description'])
+           image.append(i['urlToImage'])
+           url.append(i['url'])
+           newid.append(c)
+           content.append(i['content'])
+           break
+         c+=1
+       news = zip(title,description,image,url,newid,content)
+
+       context={
+            'news':news,
+       }
+       return render(request,'pages/new.html',context)
+   
+def newTechnology (request,new_id):
+       r = requests.get('https://newsapi.org/v2/top-headlines?category=technology&country=in&apiKey=bbb83246743a4927bee52dcc7e7fb48e')
+       res = r.json()
+       data = res['articles']
+       title = []
+       description = []
+       image = []
+       url = []
+       newid=[]
+       content=[]
+       c=0
+       for i in data:
+         if(int(new_id)==c):
+           title.append(i['title'])
+           description.append(i['description'])
+           image.append(i['urlToImage'])
+           url.append(i['url'])
+           newid.append(c)
+           content.append(i['content'])
+           break
+         c+=1
+       news = zip(title,description,image,url,newid,content)
+
+       context={
+            'news':news,
+       }
+       return render(request,'pages/new.html',context)
+   
+def newEntertainment (request,new_id):
+       r = requests.get('https://newsapi.org/v2/top-headlines?category=entertainment&country=in&apiKey=bbb83246743a4927bee52dcc7e7fb48e')
+       res = r.json()
+       data = res['articles']
+       title = []
+       description = []
+       image = []
+       url = []
+       newid=[]
+       content=[]
+       c=0
+       for i in data:
+         if(int(new_id)==c):
+           title.append(i['title'])
+           description.append(i['description'])
+           image.append(i['urlToImage'])
+           url.append(i['url'])
+           newid.append(c)
+           content.append(i['content'])
+           break
+         c+=1
+       news = zip(title,description,image,url,newid,content)
+
+       context={
+            'news':news,
+       }
+       return render(request,'pages/new.html',context)
+   
+def newSearch (request,new_id,name):
+       SEARCH_NEWS = ('https://newsapi.org/v2/everything?apiKey=bbb83246743a4927bee52dcc7e7fb48e&q=')
+       s=SEARCH_NEWS+str(name)
+       r = requests.get(s)
+       res = r.json()
+       data = res['articles']
+       title = []
+       description = []
+       image = []
+       url = []
+       newid=[]
+       content=[]
+       c=0
+       for i in data:
+         if(int(new_id)==c):
+           title.append(i['title'])
+           description.append(i['description'])
+           image.append(i['urlToImage'])
+           url.append(i['url'])
+           newid.append(c)
+           content.append(i['content'])
+           break
+         c+=1
+       news = zip(title,description,image,url,newid,content)
+
+       context={
+            'news':news,
+       }
+       return render(request,'pages/new.html',context)
